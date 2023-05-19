4cf0321c6aa64ead4405f8d4234a2fb8edcbfa4e
Valentin Chauvet <valentinchauvet.contact@gmail.com>
2023-05-19 14:58:44 +0200

    Header Hover Color coral

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,16 +18,16 @@ export default function Header() {
             <div className="w-full text-white font-bold text-sm flex-2">
                 <nav className="h-full">
                     <ul className="h-full flex flex-col items-center justify-center w-full p-4 space-y-4">
-                        <li className="hover:text-yellow hover:scale-110 ease-in-out duration-300 p-2">
+                        <li className="hover:text-coral hover:scale-110 ease-in-out duration-300 p-2">
                             <Link href="/">HOME</Link>
                         </li>
-                        <li className="hover:text-yellow hover:scale-110 ease-in-out duration-300 p-2">
+                        <li className="hover:text-coral hover:scale-110 ease-in-out duration-300 p-2">
                             <Link href="/gallery">GALLERY</Link>
                         </li>
-                        <li className="hover:text-yellow hover:scale-110 ease-in-out duration-300 p-2">
+                        <li className="hover:text-coral hover:scale-110 ease-in-out duration-300 p-2">
                             <Link href="/">PRICING</Link>
                         </li>
-                        <li className="hover:text-yellow hover:scale-110 ease-in-out duration-300 p-2">
+                        <li className="hover:text-coral hover:scale-110 ease-in-out duration-300 p-2">
                             <Link href="/">CONTACT</Link>
                         </li>
                     </ul>
